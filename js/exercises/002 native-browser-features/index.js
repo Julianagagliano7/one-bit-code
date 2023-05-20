612a31f3f75a49cd81d35729fa7739210eaba1ba
Juliana Gagliano <julianagagliano1@gmail.com>
2023-05-20 16:57:30 -0300

    native browser features

---
+++ js/exercises/002 native-browser-features/index.js
@@ -0,0 +1,15 @@
+let name = "Juliana Gagliano";
+
+console.log(name);
+console.error(name); // erro
+console.warn(name); // aviso
+
+//window.alert(name);   // alerta fora do console com a mensagem
+//name = window.prompt('Insira seu nome:');
+//let confirmacao = window.confirm('Confirma essa ação?'); //abre um model de confirmação
+
+//Window é um comando 'coringa', então basta eu colocar só o nome do comando
+
+alert('Preste Atenção!'); 
+prompt('Digite seu nome:'); 
+confirm('Confirme seus dados');
