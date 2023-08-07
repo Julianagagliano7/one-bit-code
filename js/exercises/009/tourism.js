e63f7b303f20a649565c32e136c7e344243cc0c9
Juliana Gagliano <julianagagliano1@gmail.com>
2023-08-07 07:25:59 -0300

    using arrays to solve exercise 009 - better solution

--- js/exercises/009/tourism.js
+++ js/exercises/009/tourism.js
@@ -1,22 +1,29 @@
+//nome do turista 
+// nº de cidades que visitou
+// quais cidades visitou 
+
 const name = prompt('What is your name?');
 let isVisited = prompt('Have you ever been to a city before? yes/no');
-let cities = ''; 
+let cities = []; 
 let counterCity = 0; 
-
+let i = 0; 
 
 while (isVisited === 'yes'){
   counterCity++; 
 
   let cityName = prompt('Type down the name of the city:');
-  cities += ' \n- ' + cityName + '\n';
+  cities.push(cityName);
 
   isVisited = prompt('Have you ever visited another city? yes/no');
 }
 
 alert('O nome do turista é: ' + name);
 alert('O ' + name + ' visitou ' + counterCity + ' cidades');
-alert(name + ' visitou essas cidades: ' + cities); 
 
-//nome do turista 
-// nº de cidades que visitou
-// quais cidades visitou 
+while(i < cities.length){
+  alert(cities[i]);
+  i++;
+}
+
+
+
